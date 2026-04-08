6eb743080bc0597dd878d0cebdd793a2a0f5fa91
Warren Favors <warren.favors0@gmail.com>
2026-04-08 15:40:55 -0500

    changed radii values

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -3,10 +3,10 @@ def compute_area_of_circle(radius):
 	area = pi * radius * radius
 	return area
 
-radius1 = 30
+radius1 = 60
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
-radius2 = 40
+radius2 = 1000
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
